da9b92f26d44b87312f42d9047dde911fce6c121
enzo jakobasch <enzo.jakobasch@gmail.com>
2023-04-10 20:45:12 +0200

    improve

--- src/components/Background.jsx
+++ src/components/Background.jsx
@@ -2,8 +2,9 @@ import React from 'react';
 
 const Background = () => {
   return (
-    <div className="relative bg-gradient-to-r from-red-100 to-lime-50 h-screen blur-lg">
-      <div className="bg-green-500 h-32 w-32 absolute top-200 left-1000"></div>
+    <div className="relative bg-gradient-to-r from-red-100 to-lime-50 h-screen blur-2xl">
+      <div className="rounded-lg -rotate-45 bg-green-500 h-64 w-96 absolute top-1/2 right-1/3 transform translate-x-1/2 -translate-y-1/2 skew-y-12"></div>
+      <div className="rounded-full bg-pink-300 h-80 w-80 absolute top-1/2 right-2/4 transform translate-x-1/5 -translate-y-1/2"></div>
     </div>
   );
 }

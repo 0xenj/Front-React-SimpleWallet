51e9d606c539283a8164a2cf33fa45cdca843ae8
enzo jakobasch <enzo.jakobasch@gmail.com>
2023-04-12 20:51:11 +0200

    improve text

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -6,20 +6,20 @@ const Dashboard = () => {
         <h1 className="text-2xl font-extrabold mb-4">Dashboard</h1>
         <div className="flex flex-col gap-2">
           <div className="bg-gray-200 p-4 rounded-lg">
-            <h2 className="text-lg font-medium mb-2">Total funds</h2>
-            <p className="text-gray-700 font-bold">000000000000000000000</p>
+            <h2 className="text-lg font-bold mb-2">Total funds</h2>
+            <p className="text-gray-700 font-medium">000000000000000000000</p>
           </div>
           <div className="bg-gray-200 p-4 rounded-lg">
-            <h2 className="text-lg font-medium mb-2">Total funds locked</h2>
-            <p className="text-gray-700 font-bold">000000000000000000000</p>
+            <h2 className="text-lg font-bold mb-2">Total funds locked</h2>
+            <p className="text-gray-700 font-medium">000000000000000000000</p>
           </div>
           <div className="bg-gray-200 p-4 rounded-lg">
-            <h2 className="text-lg font-medium mb-2">Rewards token total supply</h2>
-            <p className="text-gray-700 font-bold">000000000000000000000</p>
+            <h2 className="text-lg font-bold mb-2">Rewards token total supply</h2>
+            <p className="text-gray-700 font-medium">000000000000000000000</p>
           </div>
           <div className="bg-gray-200 p-4 rounded-lg">
-            <h2 className="text-lg font-medium mb-2">Protocol gains</h2>
-            <p className="text-gray-700 font-bold">000000000000000000000</p>
+            <h2 className="text-lg font-bold mb-2">Protocol gains</h2>
+            <p className="text-gray-700 font-medium">000000000000000000000</p>
           </div>
         </div>
       </div>

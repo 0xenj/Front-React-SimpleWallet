38d08233e06b6cc3ae7b10ce0fb716261db0d278
enzo jakobasch <enzo.jakobasch@gmail.com>
2023-04-13 21:22:29 +0200

    improve

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -37,20 +37,18 @@ const Card = () => {
   }, []);
 
   return (
-    <div class="card">
-      <div class="card__wrapper">
-        <div class="card__3d">
-          <div class="card__image relative">
+    <div className="card">
+      <div className="card__wrapper">
+        <div className="card__3d">
+          <div className="card__image relative">
             <img src="https://img.freepik.com/free-photo/white-concrete-textures_74190-6994.jpg?size=626&ext=jpg&ga=GA1.1.1238656915.1681375468&semt=ais" alt="" />
-              <h1 className='text-2xl absolute -translate-y-52 translate-x-24 text-black font-bold'>Balance</h1>
-              <span className='text-xl absolute -translate-y-40 translate-x-24 text-black font-bold'>000</span>
+            <h1 className='text-2xl justify-center absolute -translate-y-52 translate-x-16 text-black font-bold'>Balance</h1>
+            <span className='text-xl justify-center absolute -translate-y-40 left-1/2 transform -translate-x-1/2 text-black font-bold'>00</span>
             <div className="absolute h-0.5 w-full bg-white top-24 transform -translate-y-1/2"></div>
-            <div className="absolute top-28 bottom-24 left-12 transform -translate-y-1/2 text-black font-bold items-center justify-center">
-              <h1 className='flex-col'>Staking Balance</h1>
-              <span className='flex-col'>000000000</span>
-            </div>
+            <h1 className='text-xl justify-center absolute -translate-y-28 translate-x-10 text-black font-bold'>Staking Balance</h1>
+            <span className='text-xl justify-center absolute -translate-y-20 left-1/2 transform -translate-x-1/2 text-black font-bold'>00000000</span>
           </div>
-          <div class="card__layer1"></div>
+          <div className="card__layer1"></div>
         </div>
       </div>
     </div>
